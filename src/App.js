8c4e3fbd571f614a0ad9ee7293221a2a3d875389
Zayre <thezayrex@gmail.com>
2022-06-14 23:52:07 +0200

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Apis} from "./domain/Apis";
+import Header from "./view/header/Header";
+import NewsScreen from "./view/newsScreen/NewsScreen";
+import ArchiveScreen from "./view/archiveScreen/ArchiveScreen";
+import {VM} from './viewmodel/VM';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
+
+  //Inicialización de Viewmodel y Apis.
+  Apis.initialize();
+  VM.initialize();
+
+  //Declaracion de los paths del enrutamiento.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header style={{zIndex:5}}/>
+        <Routes>
+          <Route path="/" element={<NewsScreen/>}/>
+          <Route path="/news" element={<NewsScreen/>}/>
+          <Route path="/archive" element={<ArchiveScreen/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
